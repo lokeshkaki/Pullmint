dd8a52690dc930b188d4a10a770a8293306836e4
Lokesh Kaki <lokeshkaki1@gmail.com>
2026-03-16 23:23:32 -0500

    chore: bootstrap repo-indexer package

---
+++ services/repo-indexer/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverageFrom: ['**/*.ts', '!**/__tests__/**', '!**/node_modules/**', '!**/dist/**'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: { '^.+\\.ts$': ['ts-jest'] },
+};
