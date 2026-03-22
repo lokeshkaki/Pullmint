67a04a1d8dd397ce837cf14f16f225970770ac2c
Lokesh Kaki <lokeshkaki1@gmail.com>
2026-03-22 11:14:36 -0500

    chore: remove TypeScript build artifacts from git

--- services/shared/tracing.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function initTracing(serviceName: string): void;
-export declare function addTraceAnnotations(annotations: Record<string, string | number>): void;
-//# sourceMappingURL=tracing.d.ts.map
